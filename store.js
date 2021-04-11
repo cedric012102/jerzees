3cd8f2329dfb23d7c5a406d3c211a8025b1c09da
Cedric Smith <cedricsmith@Cedrics-iMac.local>
2021-04-11 16:31:23 -0700

    updated

--- store.js
+++ store.js
@@ -3,6 +3,7 @@ console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(event) {
+        event.preventDefault();
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
     })
